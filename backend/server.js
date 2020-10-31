49ef0c24fa62ec0170db9f357d62ee1740531e16
Vignesh Manel <vnayak83@gmail.com>
2020-10-31 20:50:08 +0530

    Post message API

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Messages = require('./dbmessages.js')
 
 const app = express()
 const port = process.env.PORT || 9000
+
+app.use(express.json())
 
 const connection_url = 'mongodb+srv://admin:xA1vF1OievwsDShq@cluster0.fujwv.mongodb.net/whatsappdb?retryWrites=true&w=majority'
 
@@ -13,5 +16,17 @@ mongoose.connect(connection_url,{
 });
 
 app.get('/',(req,res) => res.status(200).send('hello world'))
+
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body;
+
+    Messages.create(dbMessage, (err, data) => {
+	if (err) {
+	    res.status(500).send(err)
+	} else {
+	    res.status(201).send(data)
+	}
+    })
+})
 
 app.listen(port, () => console.log(`listening on localhost:${port}`))
