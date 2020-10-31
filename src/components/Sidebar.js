61f8c5acd9d895d55084586e3a200daf7196c658
Vignesh Manel <vnayak83@gmail.com>
2020-10-31 13:10:24 +0530

    Sidebar Chat

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,7 @@ import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { SearchOutlined } from "@material-ui/icons/";
+import SidebarChat from "./SidebarChat";
 
 import { Avatar, IconButton } from "@material-ui/core"
 
@@ -31,6 +32,13 @@ function Sidebar() {
 		    <input type="text" placeholder="Search or start new chat" />
 		</div>
 	    </div>
+
+	    <div className="sidebar_chats">
+		<SidebarChat />		
+		<SidebarChat />
+		<SidebarChat />
+	    </div>
+
 	</div>
     )
 
